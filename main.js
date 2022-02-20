da88a26d3b24cd9edbe8aedacf2e279763a0bf9f
Vikasj <92643188+Vikash852699@users.noreply.github.com>
2022-02-20 13:09:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,32 @@ function preload(){
 function setup(){
 canvas= createCanvas(300, 300);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', GotPoses);
 }
 
 function draw(){
-
+  image(video,0,0,300,300);
 }
 
 function take_snapshot(){
 save("filter_selfie.png");
 }
+
+function modelLoaded(){
+    console.log("Model Loaded :)")
+    }
+    
+    function GotPoses(results){
+    
+      if( results.length > 0){
+        console.log(results);
+        console.log(results[0].pose.nose.x);
+        console.log(results[0].pose.nose.y);
+      }
+        
+    }
